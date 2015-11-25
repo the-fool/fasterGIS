041a37d8737c3197b3302bae2be32ed04078f545
Thomas Ruble <sketchbang@gmail.com>
2015-11-25 13:55:18 +0000

    bak users

--- app/database.py
+++ app/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+import shutil
+import datetime
 
 engine = create_engine(os.environ.get('DEV_DATABASE_URL'), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -14,3 +16,8 @@ def init_db():
     import models
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
+
+def init_users():
+    t = datetime.date.today()
+    shutil.copytree(src='users', dst='users.bak/{0}'.format(t))
+    shutil.rmtree('users')
