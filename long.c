fa2946cbf93f7e9ac4e5b1945737c3223879bdd3
Thomas Ruble <sketchbang@gmail.com>
2015-11-29 23:05:43 +0000

    emend long.c

--- long.c
+++ long.c
@@ -8,91 +8,100 @@
 #define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
 #define MAX_BUFF 4096
 
-int do_slave(int rank);
+int do_slave(int rank, int iterations);
 int do_master();
 pid_t exec_script(int *pfd);
-int manage_script(int rank);
+int manage_script(int rank, int iterations);
 
 int main(int argc, char *argv[]) {
 
-  int world_size, world_rank;
+  int comm_size, comm_rank;
 
   MPI_Init(NULL, NULL);
 
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
 
-  if (world_rank == 0)
+  if (comm_rank == 0)
     do_master();
   else
-    do_slave(world_rank);
+    do_slave(comm_rank, 1);
  
   MPI_Finalize();
 }
 
 
-int PAUSED = 0;
-
-void stop_py(int signum) {
-  printf("caught sig %d\n", signum);
-  PAUSED = !PAUSED;
+int DORMANT = 0;
+void go_dormant(int signum) {
+  DORMANT = !DORMANT;
+  printf("caught sig %d, d=%d\n", signum, DORMANT);
 }
 
-int do_slave(int rank) {
+int do_slave(int rank, int iterations) {
   pid_t pid;
-  char pybuff[MAX_BUFF], mpibuff[MAX_BUFF];
+  char buff[MAX_BUFF];
   
   if ((pid=fork()) == -1)
     die("pipe");
   if (pid == 0) {
-    manage_script(rank);
+    manage_script(rank, iterations);
   } else {
     // MPI msg listener
     MPI_Status status;
     while (1) {
-      MPI_Recv(&mpibuff, MAX_BUFF, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-      printf("Proc %d: %s\n", rank, mpibuff);
-      
-      if (strcmp(mpibuff, "sigusr") == 0) {
+      MPI_Recv(&buff, MAX_BUFF, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+         
+      if (strcmp(buff, "sigusr") == 0) {
 	kill(pid, SIGUSR1);
       }
     }
   }
 }
 
-int manage_script(int rank){
-  int pfd[2], nbytes; 
+// to balance load with potentially many MPI processes,
+// nodes may go dormant, where they kill their computation,
+// but may begin computing agoin
+
+int manage_script(int rank, int iterations){
+  int pfd[2], nbytes, i=0; 
   char buff[MAX_BUFF];
   pid_t pid;
 
-  // first, execute computation script
-  // second, set up dedicated logger  
-  pid = exec_script(pfd);
-
   struct sigaction act;
-  act.sa_handler = stop_py;
+  act.sa_handler = go_dormant;
   sigemptyset(&act.sa_mask);
   act.sa_flags = 0;
   sigaction(SIGUSR1, &act, 0);
-    
-  while (!PAUSED) {
-    while (!PAUSED && (nbytes = read(pfd[0], buff, MAX_BUFF)) > 0) {
+  
+  while (!DORMANT && (i++ < iterations) ) {
+    pid = exec_script(pfd); 
+    while (!DORMANT && (nbytes = read(pfd[0], buff, MAX_BUFF)) > 0) {
       buff[nbytes] = '\0';
-      printf("Python proc %d: %s\n", rank, buff);
+      printf("Proc %d: %s\n", rank, buff);
+    } 
+    close(pfd[0]);
+    // check if EOF or SIGUSR1
+    if (nbytes !=0) {
+      kill(pid, SIGINT);
+      waitpid(pid, NULL, 0);
+      pause();
+    } else {
+      waitpid(pid, NULL, 0);
     }
-    //kill(pypid, SIGINT);
-    pause();
   }
 } 
 
 pid_t exec_script(int *pfd) {
   pid_t pid;
-
-  if (pipe(pfd) == -1)
+ 
+  fflush(stdout);
+ 
+ if (pipe(pfd) == -1)
     die("pipe");
   if ((pid=fork()) == -1)
     die("fork");
   if (pid == 0) {
+    signal(SIGUSR1, SIG_DFL);
     dup2(pfd[1], STDOUT_FILENO);
     close(pfd[0]); 
     close(pfd[1]);
@@ -108,7 +117,6 @@ int do_master() {
   char buff[MAX_BUFF];
   char *cptr;
   
-  printf("Send a msg: \n");
   while (1) {
     fgets(buff, MAX_BUFF, stdin);
     if ( (cptr = strchr(buff, '\n')) != NULL) *cptr = '\0';
