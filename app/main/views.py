ef9e735afa9e5d4189e9982201afe7bc8a7fbe0a
Thomas Ruble <sketchbang@gmail.com>
2015-11-30 05:07:56 +0000

    wire up create task button

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,10 @@ from flask import render_template, redirect, url_for, abort, flash, jsonify, req
 from flask.ext.login import login_required, current_user
 from . import main
 from .forms import AddTaskForm
-from .. import db
+from ..database import db_session as sess
 from ..models import User, Task
 from ..scripts import simul_types
-
+from ..tasks import celtasks as cel
 
 
 @main.route('/')
@@ -15,11 +15,17 @@ def index():
 
 @main.route('/user/<username>', methods=['GET', 'POST'])
 def user(username):
-    add_task_form = AddTaskForm()
-    add_task_form.simul_type.choices = simul_types 
+    form = AddTaskForm()
+    form.simul_type.choices = simul_types 
+    if form.validate_on_submit():
+        cel.create_simulation(form)
+        return redirect(url_for('.user', username=username))
+    elif request.method =='POST': 
+        flash('Invalid field on creation form')
+        return redirect(url_for('.user', username=username))
     user = User.query.filter_by(username=username).first()
     if user and current_user.id == user.id:
-        return render_template('dashboard.html', user=user)
+        return render_template('dashboard.html', user=user, form=form)
     elif user:
         return render_template('public_user.html', user=user)
     else:
--- app/tasks/celtasks.py
+++ app/tasks/celtasks.py
@@ -1,17 +1,19 @@
 from .. import celery
+from flask.ext.login import current_user
 from ..models import Task
 from ..database import db_session as sess
 import sys
 import os
 from subprocess import Popen, PIPE, STDOUT
 from celery.signals import task_revoked
+from datetime import datetime
 
 @celery.task(bind=True)
 def iterative_simulation(self):
-    task = self.AsyncResult(self.request.id)
     cwd = os.getcwd()
-    proc = Popen(['/usr/bin/mpirun','-n', '2', '{0}/app/mpi/simulation'.format(cwd)], stdin=PIPE,
-                 stdout=PIPE, stderr=STDOUT)
+    proc = Popen(['/usr/bin/mpirun','-n', '2', 
+                  '{0}/app/mpi/simulation'.format(cwd)], 
+                 stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     while proc.poll() is None:
         t = Task.query.filter(Task.task_id == self.request.id).first()
         
@@ -34,3 +36,11 @@ def iterative_simulation(self):
 @task_revoked.connect
 def simul_revoked(*args, **kwargs):
     print "I was revoked in celtask"
+
+def create_simulation(form):
+    task = iterative_simulation.apply_async()
+    sess.add(Task(task_id=task.id, status=task.status, 
+                  user_id=current_user.get_id(), name=form.name.data,
+                  date_begun=datetime.now()))
+    sess.commit()
+
