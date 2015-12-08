553f8d3ca314a18e74a608f82a6700d67512e46b
Thomas Ruble <sketchbang@gmail.com>
2015-12-08 15:33:32 +0000

    add a line of doc

--- app/scripts/__init__.py
+++ app/scripts/__init__.py
@@ -1,3 +1,5 @@
+"""Configuration globals for the simulations package"""
+ 
 simul_types = [('SSS', 'Sequential Sleeper Simulation'),
                ('MNDL', 'Mandelbrot Maker'),
                ('SGS', 'Sequential Gaussian Simulation')]
